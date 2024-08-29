b34b23f85d6070ef69bdd2e944416296f3a1abb5
CCMKarthik123 <chinnamuniakarthik.c@syncfusion.com>
2024-08-29 16:03:49 +0530

    resolved grid not found issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,6 @@
 
 
 import * as React from 'react';
-import * as ReactDom from 'react-dom';
-import { ColumnDirective, ColumnsDirective, GridComponent } from '@syncfusion/ej2-react-grids';
-import {
-  PdfViewerComponent, Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView,
-  ThumbnailView, Print, TextSelection, Annotation, TextSearch, FormFields, FormDesigner, Inject
-} from '@syncfusion/ej2-react-pdfviewer';
 import PDFViewer from './pdfviewer';
 
 
